19d00cfba9a793eb60e97ac84f47b138c745cb03
MartinKlasson <tillmartin2000@yahoo.se>
2020-08-14 16:41:33 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/martinklasson/jpafirstlookcodeshow/JpaFirstlookCodeshowApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.martinklasson.jpafirstlookcodeshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFirstlookCodeshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaFirstlookCodeshowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/martinklasson/jpafirstlookcodeshow/JpaFirstlookCodeshowApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.martinklasson.jpafirstlookcodeshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaFirstlookCodeshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
